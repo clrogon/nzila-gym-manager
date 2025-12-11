f828088e903707b91aab3fecea5d86210932131e
Cláudio Roberto Gonçalves <clrogon@gmail.com>
2025-12-11 23:46:31 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,11 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+<Route
+  path="/dashboard"
+  element={
+    <ProtectedRoute>
+      <Dashboard />
+    </ProtectedRoute>
+  }
+/>
