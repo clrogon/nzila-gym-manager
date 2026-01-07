1d8bda38b55259df2d74848eb8e59ce184dcc0ae
Cláudio Roberto Gonçalves <clrogon@gmail.com>
2026-01-07 17:35:25 +0100

    Vercel 404 NOT_FOUND Error - FIXED

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // Ensure base path is root for proper routing
   server: {
     host: "::",
     port: 8080,
